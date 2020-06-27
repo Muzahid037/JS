a198eebf5228493256eb01f5450e5adfca407481
Muzahid037 <48602232+Muzahid037@users.noreply.github.com>
2020-06-27 15:00:39 +0600

    27/06/20

---
+++ TYPESCRIPT/main.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var name = "Muzahid";
+var sentence = "My name is " + name + "\nI am new in typescript";
+var list1 = [1, "muzahid"];
+var Color;
+(function (Color) {
+    Color[Color["red"] = 7] = "red";
+    Color[Color["green"] = 8] = "green";
+    Color[Color["blue"] = 9] = "blue";
+})(Color || (Color = {}));
+;
+var c = Color.green;
+var myVar = 10;
+myVar = "mu";
+myVar = true;
+console.log(myVar.name);
---
+++ TYPESCRIPT/main.ts
@@ -0,0 +1,18 @@
+export {}
+let name: string = `Muzahid`;
+let sentence: string = `My name is ${name}
+I am new in typescript`;
+
+let list1: [number,string] = [1,`muzahid`];
+s
+enum Color {red=7,green,blue};
+
+let c =Color.green;
+
+let myVar: any = 10;
+myVar="mu";
+myVar=true;
+myVar();
+
+
+//console.log(myVar);
